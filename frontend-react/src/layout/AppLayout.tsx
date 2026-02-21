ede554b236b797cfb722c765c8e7b7509fec68fb
Pedro Morsch <pedro.gmorsch@gmail.com>
2026-02-21 15:01:41 -0300

    ajuste

--- frontend-react/src/layout/AppLayout.tsx
+++ frontend-react/src/layout/AppLayout.tsx
@@ -16,6 +16,20 @@ export function AppLayout() {
           <Typography variant="h6" sx={{ flexGrow: 1 }}>
             TCC - Users
           </Typography>
+
+          <Button color="inherit" component={RouterLink} to="/users">
+            Lista
+          </Button>
+          <Button color="inherit" component={RouterLink} to="/users/new">
+            Criar
+          </Button>
+
+          <Button color="inherit" component={RouterLink} to="/eventos">
+            Eventos
+          </Button>
+          <Button color="inherit" component={RouterLink} to="/eventos/new">
+            Criar Evento
+          </Button>
         </Toolbar>
       </AppBar>
 
