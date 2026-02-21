995abb5ae1639202710d7aa53e29813a22c80890
Pedro Morsch <pedro.gmorsch@gmail.com>
2026-02-21 15:03:33 -0300

    ajuste

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -50,14 +50,19 @@
 // }
 
 // export default App;
-import { Navigate, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import { HomePage } from "./pages/HomePage";
 
 export default function App() {
+  const location = useLocation();
+
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </AnimatePresence>
   );
 }
