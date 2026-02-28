bbd7f5bfbfb8ff516f1900e461c155e449852f46
cheresanford <61995917+cheresanford@users.noreply.github.com>
2026-02-28 20:08:08 -0300

    feat: keep only package.json and main.js changes

--- frontend-vue/src/main.js
+++ frontend-vue/src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue';
 import router from './router';
 import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
-import { aliases, mdi } from 'vuetify/iconsets/mdi-svg';
+import {aliases, mdi} from "vuetify/lib/iconsets/mdi"
+   import "@mdi/font/css/materialdesignicons.css"; // Ensure you are using css-loader
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 
